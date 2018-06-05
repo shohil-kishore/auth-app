42c9404dad3a98428c3505baf198c4dcb5fc5292
shohil-kishore <s.kishore@auckland.ac.nz>
2018-06-05 15:43:17 +1200

    Registration system completed

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const config = reuqire("../models/database");
+const config = require("../config/database");
 
 // User schema
 const UserSchema = mongoose.Schema({
@@ -23,4 +23,21 @@ const UserSchema = mongoose.Schema({
 
 const User = (module.exports = mongoose.model("User", UserSchema));
 
-module.exports.getUserById = function() {};
+module.exports.getUserById = function(id, callback) {
+  User.findById(id, callback);
+};
+
+module.exports.getUserByUsername = function(username, callback) {
+  const query = { username: username };
+  User.findOne(query, callback);
+};
+
+module.exports.addUser = function(newUser, callback) {
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      newUser.password = hash;
+      newUser.save(callback);
+    });
+  });
+};
